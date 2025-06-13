ca58114ec801b85ee0554272b5f7b632e8ff8d01
Vik Chadha <vikchadha@gmail.com>
2025-06-12 23:22:01 -0400

    Fix: Safely handle missing frontmatter in MDX utils

--- components/mdx/utils.ts
+++ components/mdx/utils.ts
@@ -17,17 +17,23 @@ type Metadata = {
 function parseFrontmatter(fileContent: string) {
   const frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
   const match = frontmatterRegex.exec(fileContent);
-  const frontMatterBlock = match![1];
-  const content = fileContent.replace(frontmatterRegex, "").trim();
-  const frontMatterLines = frontMatterBlock.trim().split("\n");
+  let content = fileContent;
   const metadata: Partial<Metadata> = {};
 
-  frontMatterLines.forEach((line) => {
-    const [key, ...valueArr] = line.split(": ");
-    let value = valueArr.join(": ").trim();
-    value = value.replace(/^['"](.*)['"]$/, "$1"); // Remove quotes
-    metadata[key.trim() as keyof Metadata] = value;
-  });
+  if (match && match[1]) {
+    const frontMatterBlock = match[1];
+    content = fileContent.replace(frontmatterRegex, "").trim();
+    const frontMatterLines = frontMatterBlock.trim().split("\n");
+    
+    frontMatterLines.forEach((line) => {
+      const [key, ...valueArr] = line.split(": ");
+      if (key) {
+        let value = valueArr.join(": ").trim();
+        value = value.replace(/^['"](.*)['"]$/, "$1"); // Remove quotes
+        metadata[key.trim() as keyof Metadata] = value;
+      }
+    });
+  }
 
   return { metadata: metadata as Metadata, content };
 }
